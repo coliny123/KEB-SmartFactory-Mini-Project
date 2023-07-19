949ae0dd8759810bbe7a1dc5fd7b52e64ad348e2
dohwan <dh990921@naver.com>
2023-07-19 15:15:16 +0900

    webserver v0.3 temperature

--- web_server/webserver/webserver.ino
+++ web_server/webserver/webserver.ino
@@ -10,11 +10,28 @@ const char* password = "inha4885";         // wifi pwd
 OLED_U8G2 oled; // create oled object
 WebServer server(80);      // Create a global object for use on the webserver, setting up ports
 
+int tempSensor = A2; // temperature sensor
+// formula
+int Vo;
+double R1 = 10000;
+double logR2, R2, T, Tc;
+double c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
+double Tf = 0;
 
 void handleRootEvent() {
   Serial.println("main page");
   // status code 200(ok), format, message
-  server.send(200, "text/plain", "Welcome SmartFactory WebServer!"); // Sending from server object!!!!!
+  Vo = analogRead(tempSensor); // read from temperature sensing value
+  R2 = R1 * (4095.0 / (float)Vo - 1.0);
+  logR2 = log(R2);
+  T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
+  Tc = T - 273.15;  // celsius
+  Tf = (Tc * 9.0/5.0) + 32.0;  // fahrenheit
+
+  String message = "Welcome SmartFactory WebServer!\n\n";
+  message = message + "Temperature: " + String(Tc) + "C " + "(" + String(Tf) + "F)";
+  server.send(200, "text/plain", message);  // status code 200(OK), format, message
+  Serial.println(message); // monitoring
 }
 
 
